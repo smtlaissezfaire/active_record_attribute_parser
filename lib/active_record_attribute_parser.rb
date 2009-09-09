e7434b9e5c0f944882b0104deed9b9f2133c14ec
Scott Taylor <scott@railsnewbie.com>
2009-09-09 02:01:50 -0400

    Remove usage of using gem

--- lib/active_record_attribute_parser.rb
+++ lib/active_record_attribute_parser.rb
@@ -1,10 +1,6 @@
-require "using"
-
 module ActiveRecordAttributeParser
-  extend Using
-  
-  using :Version
-  using :Parser
+  require File.expand_path(File.dirname(__FILE__) + "/active_record_attribute_parser/version")
+  require File.expand_path(File.dirname(__FILE__) + "/active_record_attribute_parser/parser")
   
   def self.parse(hash)
     Parser.parse(hash)
