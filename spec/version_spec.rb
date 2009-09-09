f93e63b8284548d73cae2978b6ad07aabeac8340
Scott Taylor <scott@railsnewbie.com>
2009-09-09 01:01:41 -0400

    Version bump to 0.1.0

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,16 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe ActiveRecordAttributeParser do
   describe "VERSION" do
-    it "should be at 0.0.0" do
-      ActiveRecordAttributeParser::Version.string.should == "0.0.0"
+    it "should be at 0.1.0" do
+      ActiveRecordAttributeParser::Version.string.should == "0.1.0"
     end
     
     it "should have major as 0" do
       ActiveRecordAttributeParser::Version.major.should == 0
     end
     
-    it "should have minor as 0" do
-      ActiveRecordAttributeParser::Version.minor.should == 0
+    it "should have minor as 1" do
+      ActiveRecordAttributeParser::Version.minor.should == 1
     end
     
     it "should have tiny as 0" do
